98c29cead04cf58be9612dd507e7a98b6f58a1a8
Sérgio <123214032+experimental-dev@users.noreply.github.com>
2023-02-02 18:51:55 -0300

    ai vai de novo

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 /**
  * index of project
 */
+class x73657276_72756c65{
+    /**
+     * @name x73657276_72756c65
+     * @param {Request} x73
+     * @param {Response} x72
+    */
+    constructor(x73,x72){
+        /**
+         * @type {Request}
+         * @returns {Request}
+        */
+        this.i73 = x73;
+        /**
+         * @type {Response}
+         * @returns {Response}
+        */
+        this.i72 = x72
+        if(){}
+    }
+    GET(){}
+    POST(){}
+    DELETE(){}
+    PUT(){}
+}
