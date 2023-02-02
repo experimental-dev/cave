e9d8529d20864c262c08ab795c45a0f76048cdba
Sérgio <123214032+experimental-dev@users.noreply.github.com>
2023-02-02 19:07:28 -0300

    integrate rules to server

--- main.control.js
+++ main.control.js
@@ -1,4 +1,9 @@
 const http = require('http');
 const x68747470 = require('\x68\x74\x74\x70');
-const x736572766572 = x68747470.createServer((s,r)=>{r.end('foo')});
+const x73657276_72756c65 = require("./x73657276_72756c65.js");
+const x736572766572 = x68747470.createServer(
+    (s,r)=>{
+        var ruse = new x73657276_72756c65(s,r);
+    }
+    );
 x736572766572.listen(8080,'localhost');
