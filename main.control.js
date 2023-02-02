431af693ccb1293dc1338c6908f2f08b78b3b4c9
Sérgio <123214032+experimental-dev@users.noreply.github.com>
2023-02-02 18:31:31 -0300

    ai vai

--- main.control.js
+++ main.control.js
@@ -1,4 +1,4 @@
 const http = require('http');
 const x68747470 = require('\x68\x74\x74\x70');
 const x736572766572 = x68747470.createServer((s,r)=>{r.end('foo')});
-x736572766572.listen(80,'localhost');
+x736572766572.listen(8080,'localhost');
