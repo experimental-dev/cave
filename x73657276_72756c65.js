04e85cd31d169dbf2fbfd6b515e2deee2ef0c86c
Sérgio <123214032+experimental-dev@users.noreply.github.com>
2023-02-03 15:28:01 -0300

    new rulers to GET

--- x73657276_72756c65.js
+++ x73657276_72756c65.js
@@ -36,7 +36,33 @@ class x73657276_72756c65{
      * @param {Request} s
      * @param {Response} r
     */
-    x474554(s,r){}
+    x474554(s,r){
+        switch(s.url.startsWith){
+            case "/": f686f6d65();
+            case "/\x61\x62\x6f\x75\x74/": f61626f7574();
+            case "/\x61\x64\x6d\x69\x6e/": f61646d696e();
+            case "/\x63\x6f\x6e\x74\x61\x63\x74\x73/":return "/\x63\x6f\x6e\x74\x61\x63\x74\x73/";
+            case "/\x73\x65\x74\x74\x69\x6e\x67\x73/": return "/\x73\x65\x74\x74\x69\x6e\x67\x73/";
+            case "/\x64\x62/":;
+            default: f686f6d65();
+        }
+        function f686f6d65(){
+            var m6673 = require('\x66\x73');
+            var v66696c6574797065 = ['\x2e\x61\x61\x63'];
+            try{
+                for(let dhcp = 0; dhcp < v66696c6574797065.length; dhcp++){
+                    if(s.url.endsWith==v66696c6574797065[dhcp]){}
+                }
+            }catch(v66696c6574797065Error){
+                console.log(v66696c6574797065Error);
+            }finally{
+                if(v66696c6574797065Error!=null,undefined){}
+            }
+        }
+        function f61626f7574(){}
+        function f61646d696e(){}
+
+    }
     /**
      * @author @experimental-dev
      * @param {Request} s
@@ -49,5 +75,6 @@ class x73657276_72756c65{
      * @param {Response} r
     */
     x505554(){s,r}
+    x667470(){}
 }
 module.exports=x73657276_72756c65;
