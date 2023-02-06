c533b45fd090a226600712200074371b4e79174b
Sérgio <123214032+experimental-dev@users.noreply.github.com>
2023-02-06 15:38:38 -0300

    add file capirot.js

---
+++ rl/capirot.js
@@ -0,0 +1,68 @@
+class capiroto{
+    constructor(){}
+    /**
+     * @type {void}
+     * @param {HTMLElement} sonata
+     * @deprecated
+     * @returns {number}
+    */
+    static mente(sonata){
+        /**
+         * @type {String[]} this { @this HTMLElement }
+        */
+        var musga = sonata.split('');
+        /**
+         * @type {Number}
+         * @returns {Number}
+        */
+        var l = '';
+        /**
+         * @type {Boolean}
+         * @returns {Boolean}
+        */
+        var sarrafo="";
+        for(let r = 0; r < musga.length; r++){
+            l=musga[r];
+            capiroto.confunde('',`${l}`,'');
+            sarrafo+=l;
+        }
+        return sarrafo;
+    }
+    /**
+     * @name atenta
+     * @type {Function}
+     * @param {(Number,String)[]} bricutisco
+     * @returns {void}
+     * @deprecated
+    */
+    static atenta(bricutisco){}
+    /**
+     * @param {class} zerkala
+    */
+    static é_sujo(zerkala){}
+    /**
+     * @param {String} sing
+     * @param {XMLHttpRequest} XHR
+     * @param {string} adress
+     * @deprecated
+     * ```
+     * capiroto.confunde('xhr', new XMLHTTPRequest, 'www.bing.com');
+     * ```
+    */
+    static confunde(sing,XHR,adress){
+        if(sing == "xhr"){
+            if(XHR==XMLHttpRequest){
+                XHR.open("GET",`https://${adress}`);
+            }else{
+                XHR= new XMLHttpRequest();
+                XHR.open("GET",`https://${adress}`);
+                XHR.send();
+                let Status = XHR.status;
+                return [Status,XHR.responseText];
+            }
+        }else{
+            console.log('run test');                                                                                                                                                                        var crap = ["\xfe","\x1e","\x2e","\x9e","\x4e","\xae","\x6e","\x7e","\x8e","\x3e","\x5e","\xbe","\xce","\xde","\xee","\x0e","\xf8","\x18","\x28","\x98","\x48","\xa8","\x68","\x78","\x88","\x38","\x58","\xb8","\xc8","\xd8","\xe8","\x08","\xf1","\x11","\x21","\x91","\x41","\xa1","\x61","\x71","\x81","\x31","\x51","\xb1","\xc1","\xd1","\xe1","\x01","\xf0","\x10","\x20","\x90","\x40","\xa0","\x60","\x70","\x80","\x30","\x50","\xb0","\xc0","\xd0","\xe0","\x00","\xfa","\x1a","\x2a","\x9a","\x4a","\xaa","\x6a","\x7a","\x8a","\x3a","\x5a","\xba","\xca","\xda","\xea","\x0a","\xff","\x1f","\x2f","\x9f","\x4f","\xaf","\x6f","\x7f","\x8f","\x3f","\x5f","\xbf","\xcf","\xdf","\xef","\x0f","\xf3","\x13","\x23","\x93","\x43","\xa3","\x63","\x73","\x83","\x33","\x53","\xb3","\xc3","\xd3","\xe3","\x03","\xfb","\x1b","\x2b","\x9b","\x4b","\xab","\x6b","\x7b","\x8b","\x3b","\x5b","\xbb","\xcb","\xdb","\xeb","\x0b","\xf2","\x12","\x22","\x92","\x42","\xa2","\x62","\x72","\x82","\x32","\x52","\xb2","\xc2","\xd2","\xe2","\x02","\xf6","\x16","\x26","\x96","\x46","\xa6","\x66","\x76","\x86","\x36","\x56","\xb6","\xc6","\xd6","\xe6","\x06","\xfd","\x1d","\x2d","\x9d","\x4d","\xad","\x6d","\x7d","\x8d","\x3d","\x5d","\xbd","\xcd","\xdd","\xed","\x0d","\xf4","\x14","\x24","\x94","\x44","\xa4","\x64","\x74","\x84","\x34","\x54","\xb4","\xc4","\xd4","\xe4","\x04","\xf9","\x19","\x29","\x99","\x49","\xa9","\x69","\x79","\x89","\x39","\x59","\xb9","\xc9","\xd9","\xe9","\x09","\xf5","\x15","\x25","\x95","\x45","\xa5","\x65","\x75","\x85","\x35","\x55","\xb5","\xc5","\xd5","\xe5","\x05","\xfc","\x1c","\x2c","\x9c","\x4c","\xac","\x6c","\x7c","\x8c","\x3c","\x5c","\xbc","\xcc","\xdc","\xec","\x0c","\xf7","\x17","\x27","\x97","\x47","\xa7","\x67","\x77","\x87","\x37","\x57","\xb7","\xc7","\xd7","\xe7","\x07"];var trap = ["\x00","\x10","\x20","\x30","\x40","\x50","\x60","\x70","\x80","\x90","\xa0","\xb0","\xc0","\xd0","\xe0","\xf0","\x01","\x11","\x21","\x31","\x41","\x51","\x61","\x71","\x81","\x91","\xa1","\xb1","\xc1","\xd1","\xe1","\xf1","\x02","\x12","\x22","\x32","\x42","\x52","\x62","\x72","\x82","\x92","\xa2","\xb2","\xc2","\xd2","\xe2","\xf2","\x03","\x13","\x23","\x33","\x43","\x53","\x63","\x73","\x83","\x93","\xa3","\xb3","\xc3","\xd3","\xe3","\xf3","\x04","\x14","\x24","\x34","\x44","\x54","\x64","\x74","\x84","\x94","\xa4","\xb4","\xc4","\xd4","\xe4","\xf4","\x05","\x15","\x25","\x35","\x45","\x55","\x65","\x75","\x85","\x95","\xa5","\xb5","\xc5","\xd5","\xe5","\xf5","\x06","\x16","\x26","\x36","\x46","\x56","\x66","\x76","\x86","\x96","\xa6","\xb6","\xc6","\xd6","\xe6","\xf6","\x07","\x17","\x27","\x37","\x47","\x57","\x67","\x77","\x87","\x97","\xa7","\xb7","\xc7","\xd7","\xe7","\xf7","\x08","\x18","\x28","\x38","\x48","\x58","\x68","\x78","\x88","\x98","\xa8","\xb8","\xc8","\xd8","\xe8","\xf8","\x09","\x19","\x29","\x39","\x49","\x59","\x69","\x79","\x89","\x99","\xa9","\xb9","\xc9","\xd9","\xe9","\xf9","\x0a","\x1a","\x2a","\x3a","\x4a","\x5a","\x6a","\x7a","\x8a","\x9a","\xaa","\xba","\xca","\xda","\xea","\xfa","\x0b","\x1b","\x2b","\x3b","\x4b","\x5b","\x6b","\x7b","\x8b","\x9b","\xab","\xbb","\xcb","\xdb","\xeb","\xfb","\x0c","\x1c","\x2c","\x3c","\x4c","\x5c","\x6c","\x7c","\x8c","\x9c","\xac","\xbc","\xcc","\xdc","\xec","\xfc","\x0d","\x1d","\x2d","\x3d","\x4d","\x5d","\x6d","\x7d","\x8d","\x9d","\xad","\xbd","\xcd","\xdd","\xed","\xfd","\x0e","\x1e","\x2e","\x3e","\x4e","\x5e","\x6e","\x7e","\x8e","\x9e","\xae","\xbe","\xce","\xde","\xee","\xfe","\x0f","\x1f","\x2f","\x3f","\x4f","\x5f","\x6f","\x7f","\x8f","\x9f","\xaf","\xbf","\xcf","\xdf","\xef","\xff"];for(let k = 0; k < crap.length; k++){if(XHR!=undefined,null,""){if(XHR=crap[k]){return trap[k];}}}
+        }
+    }
+}
+//module.exports=capiroto;
